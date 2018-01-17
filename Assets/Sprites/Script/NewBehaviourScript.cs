c0f383584d83bd1eeedc8d04b46eb4c1e457f862
FernandoHen <11hengsteber@gmail.com>
2018-01-17 08:55:07 -0600

    ...

---
+++ Assets/Sprites/Script/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log("Espacio");
+        }
+
+
+    }
+}
