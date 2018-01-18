20a2d1c6d86925326bcb54c87520f514137e3339
FernandoHen <11hengsteber@gmail.com>
2018-01-17 19:26:57 -0600

    no message

--- Assets/Sprites/Script/NewBehaviourScript.cs
+++ Assets/Sprites/Script/NewBehaviourScript.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        Debug.Log("Start");
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+public class NewBehaviourScript : MonoBehaviour
+{
+   
+    Rigidbody2D rb;
+    public float jumpForce = 0.0001f;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody2D>();
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("Espacio");
-        }
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
 
+        }
 
     }
 }
