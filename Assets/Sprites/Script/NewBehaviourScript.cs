809535ea4be39cd6fb1d5350b379f68bde0da1d3
FernandoHen <11hengsteber@gmail.com>
2018-01-23 17:42:51 -0600

    Laboratorio 1 y tarea Del Juego que nos Gusta

--- Assets/Sprites/Script/NewBehaviourScript.cs
+++ Assets/Sprites/Script/NewBehaviourScript.cs
@@ -8,23 +8,48 @@ public class NewBehaviourScript : MonoBehaviour
    
     Rigidbody2D rb;
     public float jumpForce = 0.0001f;
+    public float forwardSpeed = 2f;
+    public float sky = 0.2f;
+    public bool dead= false;
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
     }
-
+    
+    
+    
     // Update is called once per frame
     void Update()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        if (Input.GetButtonDown("Jump"))
+    {   
+
+        if (dead.Equals(false))
         {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * jumpForce);
+            rb = GetComponent<Rigidbody2D>();
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * jumpForce);
+
+            }
+
+            rb.transform.Translate(new Vector3(2, 0, 0) * forwardSpeed * Time.deltaTime);
 
         }
+        if (rb.position.x >= 53.92)
+        {
+            dead = true;
+        }
+
 
     }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dead = true;
+    }
+    
+    
+    
 }
